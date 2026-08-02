dd2b1295924bb83d94f1679243a064a978a20e23
Puri Eklavya <eklavya.puri11@gmail.com>
2026-08-02 12:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.75%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    int dp[501][501];
-
-    int solve(vector<int>& piles, int left, int right) {
-        if (left == right)
-            return piles[left];
-
-        if (dp[left][right] != -1)
-            return dp[left][right];
-
-        int takeLeft = piles[left] - solve(piles, left + 1, right);
-        int takeRight = piles[right] - solve(piles, left, right - 1);
-
-        return dp[left][right] = max(takeLeft, takeRight);
-    }
-
     bool stoneGame(vector<int>& piles) {
-        memset(dp, -1, sizeof(dp));
-        return solve(piles, 0, piles.size() - 1) > 0;
+        return true;
     }
 };
