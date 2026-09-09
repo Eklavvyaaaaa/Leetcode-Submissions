6823349d174e9effaf8359ff57e2fb4dc8ba309e
Puri Eklavya <eklavya.puri11@gmail.com>
2026-09-09 14:07:47 +0530

    Time: 3 ms (25.67%) | Memory: 17 MB (45.13%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -8,9 +8,9 @@ public:
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
                     q.push({{i, j}, 0});
                     vis[i][j] = 2;
                 }
@@ -22,7 +22,7 @@ public:
         int drow[] = {-1, 0, +1, 0};
         int dcol[] = {0, +1, 0, -1};
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
@@ -31,14 +31,14 @@ public:
 
             q.pop();
 
-            for(int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 int nrow = r + drow[i];
                 int ncol = c + dcol[i];
 
-                if(nrow >= 0 && nrow < n &&
-                   ncol >= 0 && ncol < m &&
-                   grid[nrow][ncol] == 1 &&
-                   vis[nrow][ncol] != 2){
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    grid[nrow][ncol] == 1 &&
+                    vis[nrow][ncol] != 2) {
 
                     q.push({{nrow, ncol}, t + 1});
                     vis[nrow][ncol] = 2;
@@ -46,9 +46,9 @@ public:
             }
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] != 2){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && vis[i][j] != 2) {
                     return -1;
                 }
             }
